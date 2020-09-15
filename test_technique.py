eafd7a25768689653b24cd98b72e87b9fe9a854d
SteveMoukouri <steevemouk@gmail.com>
2020-09-15 22:26:02 +0200

    Creation de compte sur le site

--- test_technique.py
+++ test_technique.py
@@ -31,4 +31,36 @@ print("le token vaut :  {}.".format(matches[0]))
 csrf=matches[0]
 print(csrf)
 
+# Demande des parametres de creation du compte :
+
+prenom = input("\n saisissez un prenom: ")
+prenom="dwfrm_profile_register_firstName=" + prenom
+nom = input("\n saisissez un nom: ")
+nom="dwfrm_profile_register_lastName=" + nom
+mail = input("\n saisissez un mail: ")
+mail_confirm="dwfrm_profile_register_emailConfirm" + mail
+mail="dwfrm_profile_register_email=" + mail
+mdp = input("\n saisissez un mdp (au moins : 1 majuscule , 1 chiffre , 1 carectere spec) :")
+mdp_confirm = "dwfrm_profile_register_passwordConfirm=" + mdp
+mdp="dwfrm_profile_register_password=" + mdp
+phone="dwfrm_profile_register_phone=&"
+birthday="dwfrm_profile_register_birthday=&"
+policy="dwfrm_profile_register_acceptPolicy=true"
+csrf_token=csrf
+param="dwfrm_profile_register_title=Herr&"+prenom+"&"+nom+"&"+mail+"&"+mail_confirm+"&"+mdp+"&"+mdp_confirm+"&"+phone+birthday+policy+"&csrf_token="+csrf
+
+#Creation du compte
+reponse = req.request('/on/demandware.store/Sites-snse-FR-Site/fr_FR/Account-SubmitRegistration',method="post",body=param,
+    headers= {
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Pragma': 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+        'content-type':'application/x-www-form-urlencoded;charset=UTF-8',
+                
+    }
+)
+print(reponse)
+
 
